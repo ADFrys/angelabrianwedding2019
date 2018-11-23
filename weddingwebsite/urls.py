403342b805cdcde50ac1c09636af5c88e37f051d
AngelaF <ADFrys@users.noreply.github.com>
2018-11-23 11:54:50 -0700

    Create a new page.

--- weddingwebsite/urls.py
+++ weddingwebsite/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home),
+    path('mainpage/', views.mainpage)
 ]
