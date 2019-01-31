3554bc78182d4d6d7719cf67aaf3e563c6d4464c
AngelaF <ADFrys@users.noreply.github.com>
2019-01-31 17:47:20 -0600

    Add guestbook and save entries to database

---
+++ eventdetails/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Event
+
+class eventForm(forms.ModelForm):
+	class Meta:
+		model = Event
+		fields = ["summary"]
--- eventdetails/views.py
+++ eventdetails/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django import forms
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .models import Event
+from django.shortcuts import render
+from .forms import eventForm
 
 def event(request):
-	events = Event.objects
-	return render(request, 'event.html', {'events':events})
+
+	if request.method =="POST":
+		form = eventForm(request.POST or None)
+
+		if form.is_valid():
+			form.save()
+			events = Event.objects
+			return render(request, 'event.html', {'events':events})
+
+	else:
+		events = Event.objects
+		return render(request, 'event.html', {'events':events})
+
+
+
 
