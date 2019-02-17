db22ff8b3da7c7301fab7ebb8ff27812730d32c3
AngelaF <ADFrys@users.noreply.github.com>
2019-02-17 14:38:41 -0600

    Change to blank instead of null

--- eventdetails/models.py
+++ eventdetails/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Event(models.Model):
 	summary = models.CharField(max_length=200)
-	name = models.CharField(max_length=50, null=True)
+	name = models.CharField(max_length=50, blank=True)
 
